e2e6fe7497f11d9a674099c4a5ac8c610f5a25b3
Phillip Kang <phillipxkang@gmail.com>
2018-11-30 16:21:50 -0800

    webserver init commit + docs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # kim-cheeze
+
+## Dependencies
+```
+sudo pip install Flask
+```
+
+## Run
+```
+FLASK_APP=hello.py flask run
+```
--- test.py
+++ test.py
@@ -3,20 +3,25 @@
 # https://www.kernel.org/doc/Documentation/input/input.txt
 
 import struct 
+import os
 
 f = open( "/dev/input/event19", "rb" ); 
 # Open the file in the read-binary mode
 # The file is linked to mouse input specific to Phil's computer
 # Change '/dev/input/event19' to your input stream to recreate it
 
-while 1:
-  data = f.read(24)  # Reads data bytes in input device stream
-  time =  struct.unpack('4I',data[:16])  # Reads first 16 bytes for time data
-  data_type = struct.unpack('2b', data[16:18]) # Reads data_type
-  code = struct.unpack('2b', data[18:20]) # Reads code
-  value = struct.unpack('4b', data[20:]) # Reads event value
+os.popen('xdg-screensaver lock')
 
-  print "Time: "+ str(time[0]) +":"+str(time[2])
-  print "Type: "+ str(data_type)
-  print "Code: "+ str(code)
-  print "Value: "+ str(value)+"\n"
+# while 1:
+#   data = f.read(24)  # Reads data bytes in input device stream
+#   time =  struct.unpack('4I',data[:16])  # Reads first 16 bytes for time data
+#   data_type = struct.unpack('2b', data[16:18]) # Reads data_type
+#   code = struct.unpack('2b', data[18:20]) # Reads code
+#   value = struct.unpack('4b', data[20:]) # Reads event value
+
+#   print "Time: "+ str(time[0]) +":"+str(time[2])
+#   print "Type: "+ str(data_type)
+#   print "Code: "+ str(code)
+#   print "Value: "+ str(value)+"\n"
+
+  
