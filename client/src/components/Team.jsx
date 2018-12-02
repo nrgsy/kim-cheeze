dd581474a07330d39ee339a88df441d59c1773c1
Jade Hayes <Jade.Hayes@dominodatalab.com>
2018-12-02 00:24:15 -0800

    * Teams button * Teams button * Teams button * Teams button * Teams button * Teams button * Teams button * Teams button

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import ChartistGraph from 'react-chartist';
+import { Col, Grid, Row } from 'react-flexbox-grid';
+import styled from 'styled-components';
 
 import {
   DropdownButton,
   MenuItem
 } from 'react-bootstrap'
 
+const StyledDropdownContainer = styled.div`
+  margin-bottom: 2em;
+  text-align: left;
+`;
+
+const StyledGraphContainer =  styled.div`
+  margin: 0 0 3em 6em;
+  width: 60%;
+`;
+
+const StyledH3 =  styled.h3`
+  text-align: center;
+  width: 70%;
+`;
+
 class Team extends Component {
 
   render() {
     var data = {
-      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+      labels: ['Phil Kang', '1', 'Thomas Pech', 'Jade Hayes', 'Nathaniel Ford', 'Jacob Birk', 'Brant Strand', 'Kripa Argwal', 'Steven Davidovitz'],
       series: [
-        [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+        [75, 70, 60, 71, 92, 94, 82, 65, 60, 80],
+        [65, 60, 40, 61, 92, 74, 92, 85, 90, 80]
       ]
     };
 
 
     var options = {
-      high: 10,
-      low: -10,
+      high: 100,
+      low: 0,
       axisX: {
         labelInterpolationFnc: function(value, index) {
           return index % 2 === 0 ? value : null;
@@ -30,23 +48,34 @@ class Team extends Component {
 
     var type = 'Line'
     return (
-<<<<<<< HEAD
-      <div>TEAM
-                        <ChartistGraph data={data} options={options} type={type} />
-=======
       <div className='container'>
-        <DropdownButton
-          title='Teams'>
-          <MenuItem eventKey="1">Launchpad</MenuItem>
-          <MenuItem eventKey="2">Apps</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="3">
-            K8s
-          </MenuItem>
-          <MenuItem eventKey="4">Platform</MenuItem>
-        </DropdownButton>
->>>>>>> * Added dropdown button
+        <Grid>
+          <Row>
+            <StyledDropdownContainer>
+              <DropdownButton
+                title='Teams'>
+                <MenuItem eventKey="1">Launchpad</MenuItem>
+                <MenuItem eventKey="2">Apps</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="3">K8s</MenuItem>
+                <MenuItem eventKey="4" active>Platform</MenuItem>
+              </DropdownButton>
+            </StyledDropdownContainer>
+            <StyledH3>Team: Platform</StyledH3>
+          </Row>
+          <Row>
+            <StyledGraphContainer>
+              <ChartistGraph
+                    className='graph'
+                    data={data}
+                    options={options}
+                    type={type}
+                    />
+            </StyledGraphContainer>
+          </Row>
+        </Grid>
       </div>
+
     );
   }
 }
