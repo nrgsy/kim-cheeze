d6100805d5af4d165ed1cbe03b0ecfd7305a1f83
Phillip Kang <phillipxkang@gmail.com>
2018-12-01 16:09:13 -0800

    real

---
+++ server/github_integration.py
@@ -0,0 +1,10 @@
+from github import Github
+
+# First create a Github instance:
+# using an access token
+g = Github("d552c470c81b529031761d6519d0ecfc075e7dff")
+
+# Then play with your Github objects:
+for repo in g.get_user().get_repos():
+  print(repo.get_pulls)
+    # print(repo.name)
