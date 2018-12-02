30abe5534689484bc0b0f47f8b93c1d8f6cc0a9f
Phillip Kang <phillipxkang@gmail.com>
2018-12-01 16:28:32 -0800

    github integration

--- server/github_integration.py
+++ server/github_integration.py
@@ -2,9 +2,28 @@ from github import Github
 
 # First create a Github instance:
 # using an access token
-g = Github("d552c470c81b529031761d6519d0ecfc075e7dff")
+g = Github("21275963ff4d633fbd89d914c7a0dcc273f23779")
 
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
-  print(repo.get_pulls)
-    # print(repo.name)
+  print(repo)
+
+  print("***")
+  for pr in repo.get_pulls(state="all"):
+    print("PR title: "+pr.title)
+    print("Submitted by: "+pr.user.login)
+
+    if pr.assignee is not None:
+      print("Assignee: "+pr.assignee.login)
+
+    if len(pr.assignees) > 0:
+      print("Assignees: "),
+      for user in pr.assignees:
+        if user is not None:
+          print(user.login),
+      print("\n")
+    
+    if pr.merged_by is not None:
+      print("Merged by: "+pr.merged_by.login)   
+
+    print("---------------------")
